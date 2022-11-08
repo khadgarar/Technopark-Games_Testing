2812d8c2a0b35eabfe03c59b2f4156e2b2530ab1
Mukhamediev Roman <ronec2002@gmail.com>
2022-11-08 15:57:58 +0300

    second part commit

--- hw5.py
+++ hw5.py
@@ -1,20 +1,28 @@
-import os,glob
+import os
+import glob
+import re
 
-
-#log_path = 'C:\logs'  'C:\logs\log.log'
-log_path=input()
+#log_path = 'C:\logs' 'C:\logs\log.log' 'C:\logs\shorts' 'C:\logs\shorts\short1.log'
+log_path=input("Please eneter the path to the logs you want to be parsed\n")
 isDir = os.path.isdir(log_path)
+full_text = []
 if isDir:
     print("is a dir")
     for filename in glob.glob(os.path.join(log_path, '*.log')):
-      with open(filename, 'r') as f:
-        text = f.read()
-        print (filename)
-        print (len(text))
+        with open(filename, 'r') as f:
+            text = f.readlines()
+            full_text.extend(text)
 else:
     print("is a file")
     with open(log_path, 'r') as f:
-        text = f.read()
-        print (log_path)
-        print (len(text))
-        
+        full_text = f.readlines()
+
+print (*full_text, sep = "\n")
+print(len(full_text))
+
+result = []
+for line in full_text:
+    if re.search(".*Error.*", line) != None:
+        result.append(line)
+
+print("\n\n\nAns:\n",*result, sep = "\n")
