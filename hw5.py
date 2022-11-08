9f53408ecf29097aede5c3f9228f2a2ca487819a
Mukhamediev Roman <ronec2002@gmail.com>
2022-11-08 15:58:35 +0300

    third part commit

--- hw5.py
+++ hw5.py
@@ -1,28 +1,36 @@
 import os
 import glob
 import re
+from progress.bar import IncrementalBar
+from tqdm import tqdm
 
-#log_path = 'C:\logs' 'C:\logs\log.log' 'C:\logs\shorts' 'C:\logs\shorts\short1.log'
+#1
 log_path=input("Please eneter the path to the logs you want to be parsed\n")
 isDir = os.path.isdir(log_path)
 full_text = []
 if isDir:
-    print("is a dir")
+    print("Inputted path is a directory")
     for filename in glob.glob(os.path.join(log_path, '*.log')):
         with open(filename, 'r') as f:
             text = f.readlines()
             full_text.extend(text)
+            print("File", filename, "read completely")
 else:
-    print("is a file")
+    print("Inputted path is a file")
     with open(log_path, 'r') as f:
         full_text = f.readlines()
+    print("File read completely")
 
-print (*full_text, sep = "\n")
-print(len(full_text))
-
+print("Proceding to search all read lines ising regex")
+#2
 result = []
-for line in full_text:
-    if re.search(".*Error.*", line) != None:
-        result.append(line)
 
-print("\n\n\nAns:\n",*result, sep = "\n")
+for line in tqdm(range(len(full_text))):
+    if re.search(".*Error.*", full_text[line]) != None:
+        result.append(full_text[line])
+#3
+print("Search completed, outputting to file")
+with open("res.log", 'w') as f:
+    print(*result, file = f, end = "")
+
+print(len(result))
